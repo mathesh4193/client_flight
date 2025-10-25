9b3d7d3c8a0981a2c4c1c29850c363de0fa02e7e
Mathesh M <mathesh4193@gmail.com>
2025-10-25 12:35:46 +0530

    Commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  API_URL: process.env.REACT_APP_API_URL || ['http://localhost:5000','https://server-flight.onrender.com/'],
   STRIPE_PUBLISHABLE_KEY: process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || 'pk_test_your_stripe_publishable_key',
   // Removed GOOGLE_CLIENT_ID as Google OAuth is no longer supported
 };
